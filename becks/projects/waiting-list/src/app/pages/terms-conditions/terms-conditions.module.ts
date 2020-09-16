ff6903e735a269345e61ef4b38a04be93c963849
David Bolaños <dabeloper@gmail.com>
2020-09-16 16:42:00 -0500

    Fixed QA issues

--- becks/projects/waiting-list/src/app/pages/terms-conditions/terms-conditions.module.ts
+++ becks/projects/waiting-list/src/app/pages/terms-conditions/terms-conditions.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { TermsConditionsPageRoutingModule } from './terms-conditions-routing.module';
-
 import { TermsConditionsPage } from './terms-conditions.page';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TermsConditionsPageRoutingModule
+    TermsConditionsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [TermsConditionsPage]
 })

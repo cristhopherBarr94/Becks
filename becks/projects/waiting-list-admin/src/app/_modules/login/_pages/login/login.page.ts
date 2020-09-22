685bdaed1791ab934a37c011b5dea36af0848dd7
jpesteban <jpesteban@indracompany.com>
2020-09-22 10:15:46 -0500

    Commit login

--- becks/projects/waiting-list-admin/src/app/_modules/login/_pages/login/login.page.ts
+++ becks/projects/waiting-list-admin/src/app/_modules/login/_pages/login/login.page.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { SHA256, SHA512 } from "crypto-js";
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from "@angular/forms";
+import { HttpService } from "src/app/_services/http.service";
 
 @Component({
-  selector: 'waiting-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "waiting-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
-
-
-
 export class LoginPage implements OnInit {
-
   public userLoginForm: FormGroup;
   public captchaStatus: boolean;
   public restartCaptcha: boolean;
   public httpError: string;
 
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {}
 
   ngOnInit() {
     this.initforms();
@@ -27,52 +29,64 @@ export class LoginPage implements OnInit {
 
   initforms() {
     this.userLoginForm = this.formBuilder.group({
-      email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(30)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(3)])
+      email: new FormControl("", [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(30),
+      ]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
     });
   }
 
   loginUser(): void {
     this.restartCaptcha = true;
     this.setCaptchaStatus(!this.restartCaptcha);
-    const email256 = SHA256(this.userLoginForm.value.user).toString();
-    console.log("LoginPage -> loginUser -> email256", email256)
+    this.httpService
+      .post("http://becks.flexitco.co/becks-back/oauth/token?_format=json", {
+        username: this.userLoginForm.controls.email.value,
+        password: this.userLoginForm.controls.password.value,
+      })
+      .subscribe((resonse) => console.log(resonse));
   }
 
   public getClassInput(item: any): string {
-    let classreturn = 'input-becks';
+    let classreturn = "input-becks";
     if (item.valid) {
-      classreturn = 'input-becks-ok';
-    }
-    else if (item.touched) {
-      classreturn = 'input-becks-error';
+      classreturn = "input-becks-ok";
+    } else if (item.touched) {
+      classreturn = "input-becks-error";
     }
     return classreturn;
   }
-
 
   public getClassInputSelect(item: any): string {
-    let classreturn = 'select-becks';
+    let classreturn = "select-becks";
     if (item.valid) {
-      classreturn = 'select-becks-ok';
-    }
-    else if (item.touched) {
-      classreturn = 'select-becks-error';
+      classreturn = "select-becks-ok";
+    } else if (item.touched) {
+      classreturn = "select-becks-error";
     }
     return classreturn;
   }
 
-  public getMessageform(item: any, name: string, min?: number, max?: number): string {
-    if (item.hasError('email') && name === 'email') {
-      return 'Ingrese una dirección de correo electrónico válida';
+  public getMessageform(
+    item: any,
+    name: string,
+    min?: number,
+    max?: number
+  ): string {
+    if (item.hasError("email") && name === "email") {
+      return "Ingrese una dirección de correo electrónico válida";
     }
-    if (item.hasError('password')) {
-      return 'Contraseña inválida'
+    if (item.hasError("password")) {
+      return "Contraseña inválida";
     }
   }
 
   public setCaptchaStatus(status) {
     this.captchaStatus = status;
   }
-
 }
---
+++ becks/projects/waiting-list-admin/src/app/_services/http.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { catchError } from 'rxjs/operators';
+import { HttpConstants } from '../_constans/HttpConstants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHeaders() {
+    return {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    };
+  }
+
+  public get(url: string, headersIn?: any): Observable<object> {
+    return this.http.get(url, { headers: headersIn, observe: 'response' }).pipe(catchError(error => this.handleError(error)));
+  }
+
+  public post(url: string, body?: any, headersIn?: any): Observable<object> {
+    if (headersIn == null) {
+      headersIn = this.getHeaders();
+    }
+    return this.http.post(url, body, { headers: headersIn, observe: 'response' }).pipe(catchError(error => this.handleError(error)));
+  }
+
+  public put(url: string, body?: any, headersIn?: any): Observable<object> {
+    return this.http.put(url, body, { headers: headersIn, observe: 'response' }).pipe(catchError(error => this.handleError(error)));
+  }
+
+  public patch(url: string, body?: any, headersIn?: any): Observable<object> {
+    return this.http.patch(url, body, { headers: headersIn, observe: 'response' }).pipe(catchError(error => this.handleError(error)));
+  }
+
+  public delete(url: string, headersIn?: any): Observable<object> {
+    return this.http.delete(url, { headers: headersIn, observe: 'response' }).pipe(catchError(error => this.handleError(error)));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === HttpConstants.UNAUTHORIZED) {
+      localStorage.clear();
+    } else if (error.status === HttpConstants.CONFLICT) {
+      console.log('mensaje incorrecto');
+    } else if (error.status === HttpConstants.INTERNAL_SERVER_ERROR || error.status === HttpConstants.SERVICE_UNAVAILABLE) {
+      console.log('mensaje de servidor error');
+    } else if (!error.ok) {
+      console.log('ERROR ::: ', error);
+    }
+    return throwError(error);
+  }
+
+
+}

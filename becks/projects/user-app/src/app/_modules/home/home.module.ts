526ce3f171cdd9fee1ff82996ec742e75cf03c67
jpesteban <jpesteban@indracompany.com>
2020-09-23 18:10:43 -0500

    login

--- becks/projects/user-app/src/app/_modules/home/home.module.ts
+++ becks/projects/user-app/src/app/_modules/home/home.module.ts
@@ -6,15 +6,18 @@ import { UtilsModule } from "../utils/utils.module";
 import { HomePage } from "./_pages/home/home.page";
 import { IonicModule } from "@ionic/angular";
 import { ReactiveFormsModule } from "@angular/forms";
+import { MatTabsModule } from "@angular/material/tabs";
+import { TabsComponent } from "./_components/tabs/tabs.component";
 
 @NgModule({
-  declarations: [HomePage],
+  declarations: [HomePage, TabsComponent],
   imports: [
     CommonModule,
     IonicModule,
     ReactiveFormsModule,
     HomeRoutingModule,
     UtilsModule,
+    MatTabsModule,
   ],
 })
 export class HomeModule {}

d028595ec95704851f9e641449a8f4103965fb4a
jpesteban <jpesteban@indracompany.com>
2020-09-23 17:37:21 -0500

    login

--- becks/projects/user-app/src/app/_modules/home/home-routing.module.ts
+++ becks/projects/user-app/src/app/_modules/home/home-routing.module.ts
@@ -9,3 +9,4 @@ const routes: Routes = [{ path: "", component: HomePage }];
   exports: [RouterModule],
 })
 export class HomeRoutingModule {}
+//
--- becks/projects/user-app/src/app/_modules/home/home.module.ts
+++ becks/projects/user-app/src/app/_modules/home/home.module.ts
@@ -5,9 +5,16 @@ import { HomeRoutingModule } from "./home-routing.module";
 import { UtilsModule } from "../utils/utils.module";
 import { HomePage } from "./_pages/home/home.page";
 import { IonicModule } from "@ionic/angular";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, HomeRoutingModule, UtilsModule, IonicModule],
+  imports: [
+    CommonModule,
+    IonicModule,
+    ReactiveFormsModule,
+    HomeRoutingModule,
+    UtilsModule,
+  ],
 })
 export class HomeModule {}

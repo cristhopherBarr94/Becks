a2844d265f8d9a3f38abff55aa4e9910f07a1a0a
jpesteban <jpesteban@indracompany.com>
2020-09-24 11:56:55 -0500

    login

--- becks/projects/user-app/src/app/_modules/home/_components/tabs/tabs.component.ts
+++ becks/projects/user-app/src/app/_modules/home/_components/tabs/tabs.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'user-tabs',
-  templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss'],
+  selector: "user-tabs",
+  templateUrl: "./tabs.component.html",
+  styleUrls: ["./tabs.component.scss"],
 })
 export class TabsComponent implements OnInit {
+  public actuallTab: number = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  selectedTab = (tab) => {
+    this.actuallTab = tab.index;
+  };
 }

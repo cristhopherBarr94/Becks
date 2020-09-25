13bc02f19285eba0e736bbf7d84fbf1e65ec7817
jpesteban <jpesteban@indracompany.com>
2020-09-25 08:37:28 -0500

    login

--- becks/projects/user-app/src/app/_modules/home/_components/section-principal-form/section-principal-form.component.ts
+++ becks/projects/user-app/src/app/_modules/home/_components/section-principal-form/section-principal-form.component.ts
@@ -39,6 +39,7 @@ export class SectionPrincipalFormComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.initforms();
+    this.fbLibrary();
   }
 
   ngAfterViewInit(): void {
@@ -130,6 +131,29 @@ export class SectionPrincipalFormComponent implements OnInit, AfterViewInit {
       );
   }
 
+  loginFB() {
+    window["FB"].login(
+      (response) => {
+        console.log("login response", response);
+        if (response.authResponse) {
+          window["FB"].api(
+            "/me",
+            {
+              fields: "last_name, first_name, email",
+            },
+            (userInfo) => {
+              console.log("user information");
+              console.log(userInfo);
+            }
+          );
+        } else {
+          console.log("User login failed");
+        }
+      },
+      { scope: "email" }
+    );
+  }
+
   public moveSection() {
     const y = document.getElementById("section-img").offsetTop;
     this.principalContent.scrollToPoint(0, y);
@@ -200,5 +224,29 @@ export class SectionPrincipalFormComponent implements OnInit, AfterViewInit {
 
   public setCaptchaStatus(status) {
     this.captchaStatus = status;
+  }
+
+  fbLibrary() {
+    (window as any).fbAsyncInit = function () {
+      window["FB"].init({
+        appId: "651296645541384",
+        cookie: true,
+        xfbml: true,
+        version: "v8.0",
+      });
+      window["FB"].AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
   }
 }

60ce57f7c803e160228654939eaa440c8165273b
jpesteban <jpesteban@indracompany.com>
2020-09-25 17:19:45 -0500

    login

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit,
+} from "@angular/core";
+import { ProfilePictureComponent } from "../../_components/profile-picture/profile-picture.component";
 
 @Component({
   selector: "user-profile",
   templateUrl: "./profile.page.html",
   styleUrls: ["./profile.page.scss"],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit, AfterViewInit {
+  @ViewChild(ProfilePictureComponent) picture: ProfilePictureComponent;
+  urlPicture: string =
+    "'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'";
+
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.picture.urlImage = this.urlPicture;
+  }
 }

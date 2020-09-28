13723953e20d9ce5760052cd6f8bf483e93b0a68
jpesteban <jpesteban@indracompany.com>
2020-09-28 15:13:19 -0500

    profile

---
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/day-meter/day-meter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-day-meter',
+  templateUrl: './day-meter.component.html',
+  styleUrls: ['./day-meter.component.scss'],
+})
+export class DayMeterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

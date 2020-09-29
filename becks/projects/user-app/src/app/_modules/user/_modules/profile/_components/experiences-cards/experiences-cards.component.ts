96092bf4c999aaa69899b179ecd46d0837d5bb43
jpesteban <jpesteban@indracompany.com>
2020-09-29 11:03:04 -0500

    profile

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/experiences-cards/experiences-cards.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/experiences-cards/experiences-cards.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ExpCards } from "./mock-cards";
+import { CARD } from "./mock-cards";
 
 @Component({
-  selector: 'user-experiences-cards',
-  templateUrl: './experiences-cards.component.html',
-  styleUrls: ['./experiences-cards.component.scss'],
+  selector: "user-experiences-cards",
+  templateUrl: "./experiences-cards.component.html",
+  styleUrls: ["./experiences-cards.component.scss"],
 })
 export class ExperiencesCardsComponent implements OnInit {
+  cards = ExpCards;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }

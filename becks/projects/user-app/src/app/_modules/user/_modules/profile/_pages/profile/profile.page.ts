368913a4857be7470746bce9a96131f8297de5c3
jpesteban <jpesteban@indracompany.com>
2020-09-29 17:30:49 -0500

    home

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
@@ -12,6 +12,8 @@ import { UserService } from "src/app/_services/user.service";
 import { NameTittleComponent } from "../../_components/name-tittle/name-tittle.component";
 import { ProfilePictureComponent } from "../../_components/profile-picture/profile-picture.component";
 import { StatisticsProfileComponent } from "../../_components/statistics-profile/statistics-profile.component";
+import { Platform } from "@ionic/angular";
+import { UiService } from "src/app/_services/ui.service";
 
 @Component({
   selector: "user-profile",
@@ -22,6 +24,7 @@ export class ProfilePage implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(ProfilePictureComponent) picture: ProfilePictureComponent;
   @ViewChild(NameTittleComponent) name: NameTittleComponent;
   @ViewChild(StatisticsProfileComponent) statics: StatisticsProfileComponent;
+  public size: string;
 
   urlPicture: string =
     "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg";
@@ -32,7 +35,15 @@ export class ProfilePage implements OnInit, OnDestroy, AfterViewInit {
 
   userSubscription: Subscription;
 
-  constructor(private userSvc: UserService) {}
+  constructor(
+    private userSvc: UserService,
+    private platform: Platform,
+    private ui: UiService
+  ) {
+    platform.ready().then(() => {
+      this.size = this.ui.getSizeType(platform.width());
+    });
+  }
 
   ngOnInit() {
     this.userSubscription = this.userSvc.user$.subscribe(
--- becks/projects/user-app/src/app/_services/ui.service.ts
+++ becks/projects/user-app/src/app/_services/ui.service.ts
@@ -1,44 +1,57 @@
-import { Injectable } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { LoadingComponent } from '../_modules/utils/_components/loading/loading.component';
+import { Injectable } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { LoadingComponent } from "../_modules/utils/_components/loading/loading.component";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UiService {
-
   loading: any;
 
-  constructor( public modalCtrl: ModalController ) { }
+  constructor(public modalCtrl: ModalController) {}
 
-  async showLoading( text?: string ) {
+  async showLoading(text?: string) {
     try {
       // this.dismissLoading();
       this.loading = await this.modalCtrl.create({
         component: LoadingComponent,
-        cssClass: 'loading-modal',
+        cssClass: "loading-modal",
         animated: true,
         showBackdrop: true,
         backdropDismiss: false,
       });
       await this.loading.present();
     } catch (e) {
-      console.error( 'showLoading' , e);
+      console.error("showLoading", e);
     }
   }
 
-  dismissLoading( timeOut: number = 1000 ) {
+  dismissLoading(timeOut: number = 1000) {
     try {
-      if (this.loading) { this.loading.dismiss('cancel'); }
+      if (this.loading) {
+        this.loading.dismiss("cancel");
+      }
     } catch (e) {
-      console.error( 'dismissLoading' , e);
+      console.error("dismissLoading", e);
     }
-    setTimeout( () => {
-      this.modalCtrl.getTop().then( ( topOverlay ) => {
-        if ( topOverlay !== undefined ) {
+    setTimeout(() => {
+      this.modalCtrl.getTop().then((topOverlay) => {
+        if (topOverlay !== undefined) {
           this.modalCtrl.dismiss();
         }
       });
-    } , timeOut );
+    }, timeOut);
+  }
+
+  getSizeType(width: number) {
+    if (width < 768) {
+      return "xs";
+    } else if (width >= 768 && width < 992) {
+      return "sm";
+    } else if (width >= 992 && width < 1200) {
+      return "md";
+    } else {
+      return "lg";
+    }
   }
 }

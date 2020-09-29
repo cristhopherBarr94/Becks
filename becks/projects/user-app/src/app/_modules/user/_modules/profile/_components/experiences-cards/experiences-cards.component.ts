7b54af31184723f78dff5deddfedcd0d4b828024
jpesteban <jpesteban@indracompany.com>
2020-09-29 11:56:14 -0500

    profile

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/experiences-cards/experiences-cards.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/experiences-cards/experiences-cards.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./experiences-cards.component.scss"],
 })
 export class ExperiencesCardsComponent implements OnInit {
-  cards = ExpCardsCancel;
+  cardsCancel = ExpCardsCancel;
+  cardsPending = ExpCardsPending;
 
   constructor() {}
 
@@ -29,6 +30,21 @@ export const ExpCardsCancel: CARD[] = [
   {
     nameExperience: "dia de lluvia",
     type: "cancel",
+    urlImageExperience:
+      "https://scontent.fbog3-1.fna.fbcdn.net/v/t1.0-9/34199456_1932545923422817_3728020845444988928_n.jpg?_nc_cat=109&_nc_sid=85a577&_nc_ohc=3ncHLXVX0yEAX-d2SIV&_nc_ht=scontent.fbog3-1.fna&oh=13e31e95eb5dbc8dabcc00e20d25d68b&oe=5F9AAB71",
+  },
+];
+
+export const ExpCardsPending: CARD[] = [
+  {
+    nameExperience: "dia de playa",
+    type: "pending",
+    urlImageExperience:
+      "https://www.kayak.com.co/news/wp-content/uploads/sites/180/2018/07/Playas-baratas-en-Mexico-Cover11.jpg",
+  },
+  {
+    nameExperience: "dia de lluvia",
+    type: "pending",
     urlImageExperience:
       "https://scontent.fbog3-1.fna.fbcdn.net/v/t1.0-9/34199456_1932545923422817_3728020845444988928_n.jpg?_nc_cat=109&_nc_sid=85a577&_nc_ohc=3ncHLXVX0yEAX-d2SIV&_nc_ht=scontent.fbog3-1.fna&oh=13e31e95eb5dbc8dabcc00e20d25d68b&oe=5F9AAB71",
   },

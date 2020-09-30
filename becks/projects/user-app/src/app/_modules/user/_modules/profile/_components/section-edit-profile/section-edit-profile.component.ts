6ec02397160ba604d48e08932a52c827daf925d2
jpesteban <jpesteban@indracompany.com>
2020-09-30 10:36:50 -0500

    home

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/section-edit-profile/section-edit-profile.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/section-edit-profile/section-edit-profile.component.ts
@@ -1,4 +1,10 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
 import { Router } from "@angular/router";
 import { ProfilePictureComponent } from "../profile-picture/profile-picture.component";
 
@@ -9,12 +15,14 @@ import { ProfilePictureComponent } from "../profile-picture/profile-picture.comp
 })
 export class SectionEditProfileComponent implements OnInit, AfterViewInit {
   @ViewChild(ProfilePictureComponent) picture: ProfilePictureComponent;
+  public userEditProfileForm: FormGroup;
   public dataProfile: any;
   public urlPicture: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.initforms();
     this.dataProfile = localStorage.getItem("userData");
     this.urlPicture = this.dataProfile.urlPicture;
     console.log(
@@ -25,6 +33,30 @@ export class SectionEditProfileComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.picture.urlImage = this.urlPicture;
+  }
+
+  initforms() {
+    this.userEditProfileForm = this.formBuilder.group({
+      name: new FormControl("", [Validators.required, Validators.maxLength(3)]),
+      lastName: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      phone: new FormControl("", [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+
+  public getClassInput(item: any): string {
+    let classreturn = "input-becks";
+    if (item.valid) {
+      classreturn = "input-becks-ok";
+    } else if (item.touched) {
+      classreturn = "input-becks-error";
+    }
+    return classreturn;
   }
 
   closeEdit() {

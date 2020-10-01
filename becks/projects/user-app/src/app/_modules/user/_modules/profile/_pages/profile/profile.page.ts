2c7ba9c1f08bbe2b0bcaec2775a5951658457dad
jpesteban <jpesteban@indracompany.com>
2020-10-01 10:00:03 -0500

    home

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_pages/profile/profile.page.ts
@@ -1,17 +1,7 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  ViewChild,
-  AfterViewInit,
-  OnDestroy,
-} from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs";
 import { User } from "src/app/_models/User";
 import { UserService } from "src/app/_services/user.service";
-import { NameTittleComponent } from "../../_components/name-tittle/name-tittle.component";
-import { ProfilePictureComponent } from "../../_components/profile-picture/profile-picture.component";
-import { StatisticsProfileComponent } from "../../_components/statistics-profile/statistics-profile.component";
 import { Platform } from "@ionic/angular";
 import { UiService } from "src/app/_services/ui.service";
 import { Router } from "@angular/router";
@@ -21,7 +11,7 @@ import { Router } from "@angular/router";
   templateUrl: "./profile.page.html",
   styleUrls: ["./profile.page.scss"],
 })
-export class ProfilePage implements OnInit, OnDestroy, AfterViewInit {
+export class ProfilePage implements OnInit, OnDestroy {
   public user = new User();
   public stats = "10";
   public size: string;
@@ -64,11 +54,17 @@ export class ProfilePage implements OnInit, OnDestroy, AfterViewInit {
     this.userSubscription.unsubscribe();
   }
 
-  ngAfterViewInit() {}
-
   editProfile() {
     this.router.navigate(["user/profile/edit"], {
       queryParamsHandling: "preserve",
     });
+  }
+
+  profilePicture() {
+    return !!this.user.photo
+      ? this.user.photo
+      : this.user.gender == "female"
+      ? "../../../../../../../assets/img/profile_female.jpg"
+      : "../../../../../../../assets/img/profile_male.jpg";
   }
 }

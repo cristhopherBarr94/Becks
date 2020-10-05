009df3c2da42b4ccde095f132930dbbfc33a252d
jpesteban <jpesteban@indracompany.com>
2020-10-05 14:23:56 -0500

    profile desktop

--- becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/seccions-profile/seccions-profile.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/profile/_components/seccions-profile/seccions-profile.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { UiService } from "src/app/_services/ui.service";
 
 @Component({
-  selector: 'user-seccions-profile',
-  templateUrl: './seccions-profile.component.html',
-  styleUrls: ['./seccions-profile.component.scss'],
+  selector: "user-seccions-profile",
+  templateUrl: "./seccions-profile.component.html",
+  styleUrls: ["./seccions-profile.component.scss"],
 })
 export class SeccionsProfileComponent implements OnInit {
+  public size: string;
 
-  constructor() { }
+  constructor(private platform: Platform, private ui: UiService) {
+    platform.ready().then(() => {
+      this.platform.resize.subscribe(() => {
+        this.size = this.ui.getSizeType(platform.width());
+      });
+      this.size = this.ui.getSizeType(platform.width());
+    });
+  }
 
   ngOnInit() {}
-
 }

253c51aab5a33015db442bb5fde7e898ea560c7f
Barreto Salamanca <cybarreto@indracompany.com>
2020-10-07 11:15:31 -0500

    before change

---
+++ becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/schedule/schedule.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-schedule',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.scss'],
+})
+export class ScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- becks/projects/user-app/src/app/_modules/user/_modules/exp/exp.module.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/exp/exp.module.ts
@@ -6,9 +6,16 @@ import { HomeExpPage } from "./_pages/home-exp/home-exp.page";
 import { SliderExpComponent } from "./_components/slider-exp/slider-exp.component";
 import { VerticalCalendarComponent } from "./_components/vertical-calendar/vertical-calendar.component";
 import { UtilsModule } from "src/app/_modules/utils/utils.module";
+import { ScheduleComponent } from "./_components/schedule/schedule.component";
+import { MatCarouselModule } from "@ngmodule/material-carousel";
 
 @NgModule({
-  declarations: [SliderExpComponent, HomeExpPage, VerticalCalendarComponent],
-  imports: [CommonModule, ExpRoutingModule, UtilsModule],
+  declarations: [
+    SliderExpComponent,
+    HomeExpPage,
+    VerticalCalendarComponent,
+    ScheduleComponent,
+  ],
+  imports: [CommonModule, ExpRoutingModule, UtilsModule, MatCarouselModule],
 })
 export class ExpModule {}

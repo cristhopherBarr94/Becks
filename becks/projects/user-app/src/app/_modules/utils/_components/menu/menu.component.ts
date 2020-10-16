5ec145f6fe19e48b178c2b36ec170b1b18890269
jpesteban <jpesteban@indracompany.com>
2020-10-16 09:03:47 -0500

    reloading page

--- becks/projects/user-app/src/app/_modules/utils/_components/menu/menu.component.ts
+++ becks/projects/user-app/src/app/_modules/utils/_components/menu/menu.component.ts
@@ -37,7 +37,8 @@ export class MenuComponent implements OnInit {
     });}
 
   ngOnInit() {
-    if(!!this.urlImage){
+    console.log("MenuComponent -> ngOnInit -> this.urlImage", this.urlImage)
+    if(this.urlImage == undefined){      
       this.userSvc.getData();
       this.userSubscription = this.userSvc.user$.subscribe(
         (user: User) => {

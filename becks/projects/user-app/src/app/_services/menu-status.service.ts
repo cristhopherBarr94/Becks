b844822e708ea083ea74c2c78a9509252452fe2c
jpesteban <jpesteban@indracompany.com>
2020-10-16 10:52:43 -0500

    profile menu photo

--- becks/projects/user-app/src/app/_services/menu-status.service.ts
+++ becks/projects/user-app/src/app/_services/menu-status.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class MenuStatusService {
+  private _menuStatus = new Subject<string>();
+  public menuStatus$ = this._menuStatus.asObservable();
 
   constructor() { }
 }

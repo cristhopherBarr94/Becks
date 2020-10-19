fccbdcc9fc49fa801c42c1381071ecb345da0bb0
Barreto Salamanca <cybarreto@indracompany.com>
2020-10-19 09:16:13 -0500

    add interaction pages

--- becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/interaction-confirm/interaction-confirm.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/interaction-confirm/interaction-confirm.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { HeaderComponent } from 'src/app/_modules/utils/_components/header/header.component';
 
 @Component({
   selector: 'user-interaction-confirm',
   templateUrl: './interaction-confirm.component.html',
   styleUrls: ['./interaction-confirm.component.scss'],
 })
-export class InteractionConfirmComponent implements OnInit {
+export class InteractionConfirmComponent implements OnInit, AfterViewInit {
+  @ViewChild(HeaderComponent) header: HeaderComponent;
+  prevUrl: string = "/home";
 
   constructor() { }
 
   ngOnInit() {}
-
+  
+  ngAfterViewInit(): void {
+    this.header.urlComponent = this.prevUrl;
+  }
 }

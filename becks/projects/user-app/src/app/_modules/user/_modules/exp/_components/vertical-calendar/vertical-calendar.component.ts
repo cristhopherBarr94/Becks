88d33b6a0949e2b0a28e5a7294e5e77453e27419
Barreto Salamanca <cybarreto@indracompany.com>
2020-10-19 21:27:45 -0500

    add exp from mock

--- becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/vertical-calendar/vertical-calendar.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/vertical-calendar/vertical-calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MockExperiencias } from 'src/app/_mocks/experiencias-mock';
 import { MenuStatusService } from 'src/app/_services/menu-status.service';
 
 @Component({
@@ -7,57 +8,7 @@ import { MenuStatusService } from 'src/app/_services/menu-status.service';
   styleUrls: ["./vertical-calendar.component.scss"],
 })
 export class VerticalCalendarComponent implements OnInit {
-  public amount_exp: any = [
-    {
-      mes: "sep",
-      dia: "12",
-      titulo: "Bruno Be",
-      fecha: "Viernes 12 sep, 9pm",
-      img: "assets/img/exp-card-1.png",
-    },
-    {
-      mes: "",
-      dia: "25",
-      titulo: "Vintage",
-      fecha: "Lunes 25 oct, 9pm",
-      img: "assets/img/exp-card-2.png",
-    },
-    {
-      mes: "oct",
-      dia: "10",
-      titulo: "Beowulf",
-      fecha: "Viernes 10 oct, 9pm",
-      img: "assets/img/exp-card-3.png",
-    },
-    {
-      mes: "",
-      dia: "11",
-      titulo: "Vintage",
-      fecha: "Lunes 25 oct, 9pm",
-      img: "assets/img/exp-card-1.png",
-    },
-    {
-      mes: "",
-      dia: "12",
-      titulo: "Vintage",
-      fecha: "Lunes 25 oct, 9pm",
-      img: "assets/img/exp-card-2.png",
-    },
-    {
-      mes: "",
-      dia: "13",
-      titulo: "Vintage",
-      fecha: "Lunes 25 oct, 9pm",
-      img: "assets/img/exp-card-3.png",
-    },
-    {
-      mes: "nov",
-      dia: "10",
-      titulo: "Beowulf",
-      fecha: "Viernes 10 oct, 9pm",
-      img: "assets/img/exp-card-1.png",
-    },
-  ];
+  public amount_exp: any = MockExperiencias;
   constructor(private menuS : MenuStatusService) {}
 
   ngOnInit() {this.menuS.statusMenu("calendarmob")}

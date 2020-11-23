c4aa2dd420175bf7089f88e1099d29927b668c02
Barreto Salamanca <cybarreto@indracompany.com>
2020-11-23 16:03:18 -0500

    some issues fixed deploy 23/11

--- becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/slider-exp/slider-exp.component.ts
+++ becks/projects/user-app/src/app/_modules/user/_modules/exp/_components/slider-exp/slider-exp.component.ts
@@ -1,28 +1,37 @@
-import { AfterContentChecked, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { IonSlides, Platform } from '@ionic/angular';
-import { Subscription } from 'rxjs';
-import { Exp } from 'src/app/_models/exp';
-import { User } from 'src/app/_models/User';
-import { ExperienciasService } from 'src/app/_services/experiencias.service';
-import { UiService } from 'src/app/_services/ui.service';
-import { UserService } from 'src/app/_services/user.service';
-import { SoldMessageComponent } from 'src/app/_modules/user/_components/sold-message/sold-message.component';
-import { RedemptionsService } from 'src/app/_services/redemptions.service';
-
+import {
+  AfterContentChecked,
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { IonSlides, Platform } from "@ionic/angular";
+import { Subscription } from "rxjs";
+import { Exp } from "src/app/_models/exp";
+import { User } from "src/app/_models/User";
+import { ExperienciasService } from "src/app/_services/experiencias.service";
+import { UiService } from "src/app/_services/ui.service";
+import { UserService } from "src/app/_services/user.service";
+import { SoldMessageComponent } from "src/app/_modules/user/_components/sold-message/sold-message.component";
+import { RedemptionsService } from "src/app/_services/redemptions.service";
 
 @Component({
-  selector: 'user-slider-exp',
-  templateUrl: './slider-exp.component.html',
-  styleUrls: ['./slider-exp.component.scss'],
+  selector: "user-slider-exp",
+  templateUrl: "./slider-exp.component.html",
+  styleUrls: ["./slider-exp.component.scss"],
 })
-export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecked {
+export class SliderExpComponent
+  implements OnInit, OnDestroy, AfterContentChecked {
   public id: number;
-  public initialSlide
+  public initialSlide;
   public size: string;
-  public isActivate:boolean=false;
-  public detalleExp:boolean = false;
-  private modalIsShowed:boolean=false;
+  public isActivate: boolean = false;
+  public detalleExp: boolean = false;
+  private modalIsShowed: boolean = false;
   private codes;
   private userCodeSubs: Subscription;
   private expSubs: Subscription;
@@ -30,21 +39,21 @@ export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecke
   public experienciaContent: Exp[] = [];
   public slideOpts;
   private redemps: number[] = [];
-  public activeIndex:number;
+  public activeIndex: number;
   sliderExp = {
     isBeginningSlide: true,
     isEndSlide: false,
-    slidesItems: undefined
+    slidesItems: undefined,
   };
   truePager = false;
   aumentarTamano = false;
   itemChange: number;
   disablePrevBtn = false;
-  disableCounter:boolean;
+  disableCounter: boolean;
   disableNextBtn = false;
   wasChecked = false;
   timerToChecked = 0;
-  @ViewChild('slides') slides: IonSlides;
+  @ViewChild("slides") slides: IonSlides;
 
   constructor(
     private experienciaService: ExperienciasService,
@@ -61,74 +70,68 @@ export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecke
       this.size = this.ui.getSizeType(platform.width());
     });
 
-    this.router.events.subscribe(
-      (event) => {
-        if (event instanceof NavigationEnd) {
-          this.wasChecked = false;
-        }
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.wasChecked = false;
       }
-    );
+    });
   }
 
   ngAfterContentChecked(): void {
-    if ( !this.wasChecked ) {
+    if (!this.wasChecked) {
       window.clearTimeout(this.timerToChecked);
-      this.timerToChecked = window.setTimeout(
-        () => {
-          this.wasChecked = true;
-          this.ui.dismissLoading(0);
-          this.getIndex();
-        } , 1000
-      );
+      this.timerToChecked = window.setTimeout(() => {
+        this.wasChecked = true;
+        this.ui.dismissLoading(0);
+        this.getIndex();
+      }, 1000);
     }
   }
 
   ngOnInit() {
     this.ui.showLoading();
     const s = this.router.url;
-    this.id = Number(s.substr(s.lastIndexOf('/') + 1));
-    
+    this.id = Number(s.substr(s.lastIndexOf("/") + 1));
+
     this.experienciaContent = this.experienciaService.getActualExps();
-    this.expSubs = this.experienciaService.exp$.subscribe(response => {
-      try {
-        if ( response ) {
-          this.experienciaContent = response;
-          this.sliderExp.slidesItems = response;
-          this.slideOpts = {
-            initialSlide: this.compareId(this.id == NaN ? 0: this.id),
-            direction: "vertical",
-            speed: 400
-            
-          };
-        }
-      } catch (error) {
+    this.expSubs = this.experienciaService.exp$.subscribe(
+      (response) => {
+        try {
+          if (response) {
+            this.experienciaContent = response;
+            this.sliderExp.slidesItems = response;
+            this.slideOpts = {
+              initialSlide: this.compareId(this.id == NaN ? 0 : this.id),
+              direction: "vertical",
+              speed: 400,
+            };
+          }
+        } catch (error) {}
+        this.ui.dismissLoading();
+      },
+      (e) => {
+        this.ui.dismissLoading();
       }
-      this.ui.dismissLoading();
-    }, (e) => {
-      this.ui.dismissLoading();
-    });
-    
-    if ( !this.experienciaContent || this.experienciaContent.length==0 ) {
+    );
+
+    if (!this.experienciaContent || this.experienciaContent.length == 0) {
       this.ui.dismissLoading();
     }
     this.experienciaService.getData();
 
-    this.redempSubs = this.redempSvc.redemp$.subscribe(
-      ( red ) => {
-        this.redemps = red;
-        this.ui.dismissLoading();
-    }); 
+    this.redempSubs = this.redempSvc.redemp$.subscribe((red) => {
+      this.redemps = red;
+      this.ui.dismissLoading();
+    });
     this.redempSvc.getData();
 
-    this.userCodeSubs = this.userSvc.userCodes$.subscribe( 
-      ( codes ) => {
-        if(codes && codes.length>0){
-          this.isActivate = true;
-          this.codes = codes;
-          this.ui.dismissLoading();
-        }
+    this.userCodeSubs = this.userSvc.userCodes$.subscribe((codes) => {
+      if (codes && codes.length > 0) {
+        this.isActivate = true;
+        this.codes = codes;
+        this.ui.dismissLoading();
       }
-    );
+    });
     this.userSvc.getCodes();
   }
 
@@ -136,35 +139,46 @@ export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecke
     this.userCodeSubs.unsubscribe();
     this.expSubs.unsubscribe();
   }
-  
 
   detalleExperiencia(item: any) {
     this.itemChange = item;
-    this.experienciaContent[item].detalleExp = !this.experienciaContent[item].detalleExp;
+    this.experienciaContent[item].detalleExp = !this.experienciaContent[item]
+      .detalleExp;
     this.truePager = !this.truePager;
     if (this.aumentarTamano) {
       this.ampliarExp();
     }
   }
 
-  compareId(idExp:number){
-    for(let i=0 ; i<this.experienciaContent.length; i++){      
-      if(this.experienciaContent[i].id == idExp ){
-        return i
-      }     
+  compareId(idExp: number) {
+    for (let i = 0; i < this.experienciaContent.length; i++) {
+      if (this.experienciaContent[i].id == idExp) {
+        return i;
+      }
     }
-    return 0 
+    return 0;
   }
 
-  activarCuenta( res ) {}
+  redirectToAct(nameExp: String) {
+    this.router.navigate([`user/activation/`], {
+      queryParamsHandling: "preserve",
+    });
+    window.dataLayer.push({
+      event: "trackEvent",
+      eventCategory: "fase 3",
+      eventAction: "activa tu cuenta",
+      eventLabel: nameExp,
+    });
+  }
 
-
-  checkCodes(CurrentSld:any) {
-
-    if ( this.codes && this.codes.length > 0 ) { 
-      for(let i=0 ; i<this.experienciaContent.length; i++){      
+  checkCodes(CurrentSld: any) {
+    if (this.codes && this.codes.length > 0) {
+      for (let i = 0; i < this.experienciaContent.length; i++) {
         // this.experienciaContent[CurrentSld].status == '2' &&
-        if( this.experienciaContent[CurrentSld].stock_actual == '0' && this.modalIsShowed==false){
+        if (
+          this.experienciaContent[CurrentSld].stock_actual == "0" &&
+          this.modalIsShowed == false
+        ) {
           this.modalIsShowed = true;
           this.ui.showModal(
             SoldMessageComponent,
@@ -181,14 +195,15 @@ export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecke
   }
 
   async getIndex() {
-    if(this.slides) {
-       this.activeIndex = await this.slides.getActiveIndex();
+    if (this.slides) {
+      this.activeIndex = await this.slides.getActiveIndex();
       this.checkCodes(this.activeIndex);
-      this.disableNextBtn = ( (this.experienciaContent.length-1) == this.activeIndex );
-      if( this.activeIndex == 0){
+      this.disableNextBtn =
+        this.experienciaContent.length - 1 == this.activeIndex;
+      if (this.activeIndex == 0) {
         this.disablePrevBtn = true;
         this.disableCounter = true;
-      }else {
+      } else {
         this.disablePrevBtn = false;
         this.disableCounter = false;
       }
@@ -199,114 +214,109 @@ export class SliderExpComponent implements OnInit, OnDestroy, AfterContentChecke
     window.open("https://www.feriadelmillon.com.co/becks/");
   }
 
-  
-participateExperience(eid:number) {
-  //check redemptions
-  if ( this.redemps )  {
-    for ( let _id of this.redemps ) {
-      if ( _id == eid ) {
-        //pop up message error
-        this.router.navigate([`user/access-forbidden/${eid}`]);
-        return;
+  participateExperience(eid: number) {
+    //check redemptions
+    if (this.redemps) {
+      for (let _id of this.redemps) {
+        if (_id == eid) {
+          //pop up message error
+          this.router.navigate([`user/access-forbidden/${eid}`]);
+          return;
+        }
       }
     }
-  }
 
-  let exp = null;
-  for ( exp of this.experienciaContent ) {
-    if ( exp.id == eid ) {
-      break;
+    let exp = null;
+    for (exp of this.experienciaContent) {
+      if (exp.id == eid) {
+        break;
+      }
+    }
+
+    //notificacion message
+    // switch( this.experienciaContent[eid-1].type ) {
+    //   case "0" : break;
+    //   case "1" : break;
+    //   case "2" : break;
+    // }
+    if (exp.type == "0") {
+      this.router.navigate([`user/confirm-interaction/`], {
+        queryParamsHandling: "preserve",
+        state: { exp: exp },
+      });
+    }
+    //redirection
+    else if (exp.type == "1") {
+      window.open(exp.urlRedirect);
+    }
+    //question form
+    else if (exp.type == "2") {
+      this.router.navigate([`user/interaction/${eid}`]);
     }
   }
 
-  //notificacion message
-  // switch( this.experienciaContent[eid-1].type ) {
-  //   case "0" : break;
-  //   case "1" : break;
-  //   case "2" : break;
-  // }
-  if(exp.type == "0"){
-    this.router.navigate([`user/confirm-interaction/`], {
-      queryParamsHandling: "preserve",
-      state: { exp: exp }
+  public closeModal() {
+    this.ui.dismissModal();
+    this.modalIsShowed = false;
+  }
+
+  changeSlider() {
+    // console.log('cambio de slider # si click ->', this.itemChange);
+    if (this.itemChange !== undefined) {
+      // console.log('cambio de slider # con click ->', this.itemChange);
+      if (this.experienciaContent[this.itemChange].detalleExp === true) {
+        this.experienciaContent[this.itemChange].detalleExp = !this
+          .experienciaContent[this.itemChange].detalleExp;
+        this.truePager = !this.truePager;
+        this.itemChange = undefined;
+        if (this.aumentarTamano) {
+          this.ampliarExp();
+        }
+      }
+    }
+    this.getIndex();
+  }
+
+  ampliarExp() {
+    this.aumentarTamano = !this.aumentarTamano;
+  }
+
+  // Move to Next slide
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
     });
-
   }
-  //redirection
-  else if(exp.type == "1"){
-    window.open(exp.urlRedirect);
 
+  // Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
   }
-  //question form
-  else if(exp.type == "2"){
-    this.router.navigate([`user/interaction/${eid}`]);
 
-  }
-}
-
-public closeModal() {
-  this.ui.dismissModal();
-  this.modalIsShowed = false;
-}
-
-
-changeSlider() {
-  // console.log('cambio de slider # si click ->', this.itemChange);
-  if (this.itemChange !== undefined) {
-    // console.log('cambio de slider # con click ->', this.itemChange);
-    if (this.experienciaContent[this.itemChange].detalleExp === true) {
-      this.experienciaContent[this.itemChange].detalleExp = !this.experienciaContent[this.itemChange].detalleExp;
-      this.truePager = !this.truePager;
-      this.itemChange = undefined;
-      if (this.aumentarTamano) {
-        this.ampliarExp();
-      }
-    }
-  }
-  this.getIndex();
-}
-
-ampliarExp() {
-  this.aumentarTamano = !this.aumentarTamano;
-}
-
-// Move to Next slide
-slideNext(object, slideView) {
-  slideView.slideNext(500).then(() => {
+  // Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
     this.checkIfNavDisabled(object, slideView);
-  });
-}
+  }
 
-// Move to previous slide
-slidePrev(object, slideView) {
-  slideView.slidePrev(500).then(() => {
-    this.checkIfNavDisabled(object, slideView);
-  });
-}
+  // Call methods to check if slide is first or last to enable disbale navigation
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
 
-// Method called when slide is changed by drag or navigation
-SlideDidChange(object, slideView) {
-  this.checkIfNavDisabled(object, slideView);
-}
-
-// Call methods to check if slide is first or last to enable disbale navigation
-checkIfNavDisabled(object, slideView) {
-  this.checkisBeginning(object, slideView);
-  this.checkisEnd(object, slideView);
-}
-
-checkisBeginning(object, slideView) {
-  slideView.isBeginning().then((istrue) => {
-    object.isBeginningSlide = istrue;
-    this.disablePrevBtn = istrue;
-  });
-}
-checkisEnd(object, slideView) {
-  console.log("checkisEnd" , object);
-  slideView.isEnd().then((istrue) => {
-    object.isEndSlide = istrue;
-    this.disableNextBtn = istrue;
-  });
-}
-
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+      this.disablePrevBtn = istrue;
+    });
+  }
+  checkisEnd(object, slideView) {
+    console.log("checkisEnd", object);
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+      this.disableNextBtn = istrue;
+    });
+  }
 }

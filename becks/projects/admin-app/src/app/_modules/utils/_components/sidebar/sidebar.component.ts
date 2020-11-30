d281c04d33644587f7dc27cc47532c40b95f3251
Barreto Salamanca <cybarreto@indracompany.com>
2020-11-30 16:53:42 -0500

    lost info re-made it

--- becks/projects/admin-app/src/app/_modules/utils/_components/sidebar/sidebar.component.ts
+++ becks/projects/admin-app/src/app/_modules/utils/_components/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { Component, OnInit } from "@angular/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {}
-  
+
+  redirect(destination) {
+    this.router.navigate([`admin/` + destination]);
+  }
 }

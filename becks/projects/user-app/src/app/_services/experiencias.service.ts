0f8e68933cc566fba10952b29ca1c8ec1f0b8d12
Barreto Salamanca <cybarreto@indracompany.com>
2020-12-01 15:47:43 -0500

    add time_stamp to load images

--- becks/projects/user-app/src/app/_services/experiencias.service.ts
+++ becks/projects/user-app/src/app/_services/experiencias.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
 import { Exp } from "../_models/exp";
-import { HttpService } from './http.service';
+import { HttpService } from "./http.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class ExperienciasService {
-  
   private _exps: Exp[] = [];
   private _expSbj = new Subject<Exp[]>();
   public exp$ = this._expSbj.asObservable();
-  public   options = {
+  public options = {
     year: "numeric",
     month: "short",
     day: "numeric",
@@ -27,43 +26,61 @@ export class ExperienciasService {
   getData() {
     const urlServer = environment.serverUrl;
 
-    this.http.get( urlServer + environment.user.getExp + "?time_stamp=" + new Date().getTime() ).subscribe(
-      (response: any) => {
-        if (response.status >= 200 && response.status < 300) {
-          this._exps = [];
-          response.body.forEach((element, index) => {
-            const elementoResponse: Exp = {
-              id: element.id,
-              stock_actual: element.stock_actual,
-              detalleExp: false,
-              imagesExp: urlServer + environment.user.getImgExp + element.id + "_desk",
-              imagesExpMob: urlServer + environment.user.getImgExp + element.id + "_mob",
-              titleExp: element.title,
-              fechaExp: (new Date(element.valid_to*1000)).toLocaleString().split(" ")[0],
-              fechaAlt: element.fechaAlt,
-              detailExp: element.description,
-              placeExp: element.location,
-              urlExp: element.url_terms,
-              status: element.status,
-              horaExp: element.horaExp,
-              type:element.type,
-              urlRedirect:element.url_redirect
-            };
-            this._exps.push(elementoResponse);
-            if ( response.body.length-1 == index ) {
-              this._expSbj.next(this._exps);
-            }
-          });
-        } else {
+    this.http
+      .get(
+        urlServer +
+          environment.user.getExp +
+          "?time_stamp=" +
+          new Date().getTime()
+      )
+      .subscribe(
+        (response: any) => {
+          if (response.status >= 200 && response.status < 300) {
+            this._exps = [];
+            response.body.forEach((element, index) => {
+              const elementoResponse: Exp = {
+                id: element.id,
+                stock_actual: element.stock_actual,
+                detalleExp: false,
+                imagesExp:
+                  urlServer +
+                  environment.user.getImgExp +
+                  element.id +
+                  "_desk" +
+                  "?time_stamp=" +
+                  new Date().getTime(),
+                imagesExpMob:
+                  urlServer +
+                  environment.user.getImgExp +
+                  element.id +
+                  "_mob" +
+                  "?time_stamp=" +
+                  new Date().getTime(),
+                titleExp: element.title,
+                fechaExp: new Date(element.valid_to * 1000)
+                  .toLocaleString()
+                  .split(" ")[0],
+                fechaAlt: element.fechaAlt,
+                detailExp: element.description,
+                placeExp: element.location,
+                urlExp: element.url_terms,
+                status: element.status,
+                horaExp: element.horaExp,
+                type: element.type,
+                urlRedirect: element.url_redirect,
+              };
+              this._exps.push(elementoResponse);
+              if (response.body.length - 1 == index) {
+                this._expSbj.next(this._exps);
+              }
+            });
+          } else {
+            // TODO :: logic for error
+          }
+        },
+        (error) => {
           // TODO :: logic for error
         }
-      },
-      (error) => {
-        // TODO :: logic for error
-      }
-    );
-   
+      );
   }
-
-
 }

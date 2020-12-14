8539b1e4135d50ab8463d0688d0b8fa0067505a3
Barreto Salamanca <cybarreto@indracompany.com>
2020-12-14 15:06:55 -0500

    footer to terms and condi redirection added terms with href file added

--- becks/projects/user-app/src/app/_modules/utils/_pages/terms-conditions/terms-conditions.module.ts
+++ becks/projects/user-app/src/app/_modules/utils/_pages/terms-conditions/terms-conditions.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { TermsConditionsPageRoutingModule } from './terms-conditions-routing.module';
-import { TermsConditionsPage } from './terms-conditions.page';
-import { UtilsModule } from '../../utils.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { TermsConditionsPageRoutingModule } from "./terms-conditions-routing.module";
+import { TermsConditionsPage } from "./terms-conditions.page";
+import { UtilsModule } from "../../utils.module";
 
 @NgModule({
   imports: [
@@ -12,8 +12,8 @@ import { UtilsModule } from '../../utils.module';
     FormsModule,
     IonicModule,
     TermsConditionsPageRoutingModule,
-    UtilsModule
+    UtilsModule,
   ],
-  declarations: [TermsConditionsPage]
+  declarations: [TermsConditionsPage],
 })
 export class TermsConditionsPageModule {}
--- becks/projects/user-app/src/app/_modules/utils/_pages/terms-conditions/terms-conditions.page.ts
+++ becks/projects/user-app/src/app/_modules/utils/_pages/terms-conditions/terms-conditions.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ExperienciasService } from "src/app/_services/experiencias.service";
+import { HttpService } from "src/app/_services/http.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-terms-conditions',
-  templateUrl: './terms-conditions.page.html',
-  styleUrls: ['./terms-conditions.page.css'],
+  selector: "app-terms-conditions",
+  templateUrl: "./terms-conditions.page.html",
+  styleUrls: ["./terms-conditions.page.scss"],
 })
 export class TermsConditionsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private expSubs: Subscription;
+  public terms_exp = [];
+  public goblalUrl;
+  constructor(
+    private expService: ExperienciasService,
+    private httpService: HttpService
+  ) {
+    this.expSubs = this.expService.exp$.subscribe((exps) => {
+      if (exps && exps.length > 0) {
+        this.terms_exp = exps;
+      }
+    });
+    this.expService.getData();
   }
 
+  ngOnInit() {}
+  ngOnDestroy(): void {
+    this.expSubs.unsubscribe();
+  }
 }
